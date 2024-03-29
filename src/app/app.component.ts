afb29ab8a8257195a41cb62c80d4ed00862a174f
Elif Gökçe <e_kaya0@outlook.com>
2024-03-29 17:08:34 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ArikWebPage';
+export class AppComponent implements AfterViewInit {
+  constructor() {}
+
+  initScrollTrigger() {
+    gsap.to('.menu, .nav-list, .bottom', {
+      opacity: 0,
+      duration: 0.5,
+      scrollTrigger: {
+        trigger: 'section.home',
+        scrub: true,
+        start: '60% 60%',
+        end: '70% 30%',
+      },
+    });
+
+    gsap.to('.heading-main', {
+      opacity: 0,
+      duration: 0.5,
+      scrollTrigger: {
+        trigger: 'section.two',
+        scrub: true,
+        start: '70% 60%',
+        end: '85% 30%',
+      },
+    });
+
+    gsap.to('figure.preview', {
+      scrollTrigger: {
+        toggleClass: 'active',
+        trigger: 'figure.preview',
+        start: '50% 80%',
+        end: '85% 20%',
+      },
+    });
+  }
+
+  ngAfterViewInit() {
+    this.initScrollTrigger();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    RouterOutlet,
+    AppRoutingModule,
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule,
+    RouterModule.forRoot([], {scrollPositionRestoration: 'enabled'})
   ],
-  providers: [],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync(),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
